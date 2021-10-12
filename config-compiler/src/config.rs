27700f8caf0779804fa3e04271176f1a1101519d
Onur Ozkan <onurozkan.dev@outlook.com>
2021-10-12 15:27:23 +0300

    add log_level to configuration

--- config-compiler/src/config.rs
+++ config-compiler/src/config.rs
@@ -8,6 +8,7 @@ pub struct RuntimeConfig {
     pub worker_cores: usize,
     pub inbound_port: u32,
     pub outbound_addr: String,
+    pub log_level: u8,
 }
 
 impl Default for Config {
@@ -17,6 +18,7 @@ impl Default for Config {
                 worker_cores: 0, // 0 means use all the available threads
                 inbound_port: 6150,
                 outbound_addr: "127.0.0.1:8080".to_string(),
+                log_level: 0,
             },
         }
     }
