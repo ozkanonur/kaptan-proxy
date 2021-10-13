8af2c4f89edfa3fe5bd69d308ac40921ddf03ee6
Onur <onurozkan.dev@outlook.com>
2021-10-13 11:36:50 +0300

    update configuration path

--- config-compiler/src/compiler.rs
+++ config-compiler/src/compiler.rs
@@ -2,7 +2,7 @@ use std::fs;
 use crate::config::Config;
 
 pub fn get_configuration() -> Config {
-    let config_path: &str = "/etc/kaptan/cfg.toml";
+    let config_path: &str = "/etc/kaptan-proxy/cfg.toml";
     let config_str = fs::read_to_string(config_path)
         .expect(&format!("{} could not found.", config_path).to_owned());
 
