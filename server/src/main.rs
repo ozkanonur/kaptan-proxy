02fff1469e474f253680fb7c1a00f6bab25d00e2
Onur <onurozkan.dev@outlook.com>
2021-10-13 19:52:03 +0300

    read buffer from TcpStream

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,18 @@
 #![warn(rust_2018_idioms)]
 
+use bytes::Bytes;
 use config_compiler::compiler::*;
+use futures::{
+    self,
+    stream::{Stream, StreamExt, TryStreamExt},
+    FutureExt,
+};
 use tokio::io;
 use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncRead, Result};
 use tokio::net::{TcpListener, TcpStream};
-
-use futures::FutureExt;
-use std::error::Error;
-
+use tokio_util::codec::{BytesCodec, FramedRead};
+use tokio_util::io::StreamReader;
 mod runtime;
 
 fn main() {
@@ -34,20 +39,34 @@ fn main() {
     });
 }
 
-async fn transfer(mut inbound: TcpStream, proxy_addr: String) -> Result<(), Box<dyn Error>> {
+fn byte_to_stream<R>(r: R) -> impl Stream<Item = Result<Bytes>>
+where
+    R: AsyncRead,
+{
+    FramedRead::new(r, BytesCodec::new()).map_ok(|bytes| bytes.freeze())
+}
+
+async fn transfer(mut inbound: TcpStream, proxy_addr: String) -> Result<()> {
     let mut outbound = TcpStream::connect(proxy_addr).await?;
 
-    let (mut ri, mut wi) = inbound.split();
-    let (mut ro, mut wo) = outbound.split();
+    let (read_inbound, mut write_inbound) = inbound.split();
+    let (mut read_outbound, mut write_outbound) = outbound.split();
+
+    let cli_read_stream = byte_to_stream(read_inbound).map(|buf| {
+        println!("{:?}", buf);
+        buf
+    });
 
     let client_to_server = async {
-        io::copy(&mut ri, &mut wo).await?;
-        wo.shutdown().await
+        let mut read_inbound = StreamReader::new(cli_read_stream);
+
+        io::copy(&mut read_inbound, &mut write_outbound).await?;
+        write_outbound.shutdown().await
     };
 
     let server_to_client = async {
-        io::copy(&mut ro, &mut wi).await?;
-        wi.shutdown().await
+        io::copy(&mut read_outbound, &mut write_inbound).await?;
+        write_inbound.shutdown().await
     };
 
     tokio::try_join!(client_to_server, server_to_client)?;
