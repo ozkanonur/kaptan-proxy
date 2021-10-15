2b99887a302454e98704becbe5388ce63a3574bb
Onur <onurozkan.dev@outlook.com>
2021-10-15 12:42:06 +0300

    Dev/docs (#2)
    * Provide docs all around the codebase
    
    * update .todo tasks

--- config-compiler/src/lib.rs
+++ config-compiler/src/lib.rs
@@ -6,5 +6,23 @@ pub mod compiler;
 pub mod config;
 
 pub trait Compiler {
+    /// Reads the configurations from /etc/kaptan-proxy/cfg.toml, deserializes
+    /// it into config::Config struct and returns an instance of it.
+    ///
+    /// # Panics
+    /// If:
+    ///     - Configuration value types are incorrect.
+    ///     - **/etc/kaptan-proxy/cfg.toml** does not exists.
+    ///     - Missing permission for **/etc/kaptan-proxy/cfg.toml**.
+    ///
+    /// # Usage
+    /// ```
+    /// use config_compiler::{config::Config, Compiler};
+    ///
+    /// fn main*( {
+    ///     let config = Config::read_from_fs();
+    /// }
+    ///
+    /// ```
     fn read_from_fs() -> Config;
 }
