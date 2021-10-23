defe6448e2d3d399669c4e3531016a3540df03f6
Onur <onurozkan.dev@outlook.com>
2021-10-23 19:07:48 +0300

    Update access_log.rs

--- logger/src/access_log.rs
+++ logger/src/access_log.rs
@@ -2,12 +2,13 @@ use crate::LogCapabilities;
 use chrono::Local;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::fmt::Debug;
 
 pub struct AccessLog<'a, S> {
     pub request: &'a S,
 }
 
-impl<S> LogCapabilities for AccessLog<'_, S> where S: std::fmt::Debug {
+impl<S> LogCapabilities for AccessLog<'_, S> where S: Debug {
     fn write(&self) {
         let mut log_file = OpenOptions::new()
             .append(true)
