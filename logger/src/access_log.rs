efe3fbe0a3ba98aac2de797af49be7fb9ab5eb8d
Onur <onurozkan.dev@outlook.com>
2021-10-25 03:01:17 +0300

    Development (#5)
    finalize logging middleware implementation

--- logger/src/access_log.rs
+++ logger/src/access_log.rs
@@ -1,25 +1,45 @@
 use crate::LogCapabilities;
 use chrono::Local;
+use std::fmt::Debug;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-pub struct AccessLog {
-    pub log_message: String,
+pub struct AccessLog<'a, X, Y, Z, T> {
+    pub method: &'a X,
+    pub uri: &'a Y,
+    pub version: &'a Z,
+    pub headers: &'a T,
 }
 
-impl AccessLog {}
-
-impl LogCapabilities for AccessLog {
+impl<X, Y, Z, T> LogCapabilities for AccessLog<'_, X, Y, Z, T>
+where
+    X: Debug,
+    Y: Debug,
+    Z: Debug,
+    T: Debug,
+{
     fn write(&self) {
         let mut log_file = OpenOptions::new()
-            .append(true)
             .create(true)
+            .append(true)
+            .read(false)
             .open("access-logs")
             .expect("Unable to open file");
 
-        let formatted_log = &self.log_message.trim().replace("\r\n", " | ");
-        let local_time = Local::now();
-        let formatted_log = format!("{} -> {}\n", local_time, formatted_log);
-        log_file.write_all(formatted_log.as_bytes()).expect("stuff");
+        let mut formatted_bytes = Vec::new();
+        writeln!(
+            &mut formatted_bytes,
+            "{{\"_time\": \"{}\", \"method\": \"{:?}\", \"uri\": \"{:?}\", \"version\": \"{:?}\", \"headers\": {:?}}}",
+            Local::now().naive_local(),
+            self.method,
+            self.uri,
+            self.version,
+            self.headers
+        )
+        .unwrap();
+
+        log_file
+            .write_all(&formatted_bytes)
+            .expect("Panic raised while writing access log to the file.");
     }
 }
--- network-utils/middlewares/src/logging_middleware.rs
+++ network-utils/middlewares/src/logging_middleware.rs
@@ -1,36 +1,9 @@
-use futures::future::Future;
 use hyper::Request;
-use pin_project::pin_project;
-use std::{pin::Pin, task::Poll};
+use logger::{access_log::AccessLog, LogCapabilities};
+use std::task::Poll;
 use tower::Service;
 
-#[pin_project]
-/// Result type of tower implementation of LoggingMiddleware.
-///
-/// Created to avoid of BoxFuture implementation which
-/// causes runtime overhead, or Pinning the Result that causes
-/// inability to use async blocks.
-pub struct LoggingFuture<F> {
-    #[pin]
-    future: F,
-}
-
-impl<F> Future for LoggingFuture<F>
-where
-    F: Future,
-{
-    type Output = F::Output;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        let this = self.project();
-        let res = match this.future.poll(cx) {
-            Poll::Ready(res) => res,
-            Poll::Pending => return Poll::Pending,
-        };
-
-        Poll::Ready(res)
-    }
-}
+use crate::MiddlewareFuture;
 
 /// Middleware service that can route and proxy between
 /// two connections.
@@ -52,24 +25,29 @@ impl<S> LoggingMiddleware<S> {
 impl<S, B> Service<Request<B>> for LoggingMiddleware<S>
 where
     S: 'static + Service<Request<B>> + Clone + Send,
-    B: 'static + Send,
-    S::Future: 'static + Send
+    B: 'static + Send + std::fmt::Debug,
+    S::Future: 'static + Send,
 {
     type Response = S::Response;
 
     type Error = S::Error;
 
-    type Future = LoggingFuture<S::Future>;
+    type Future = MiddlewareFuture<S::Future>;
 
     fn poll_ready(&mut self, cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
     }
 
     fn call(&mut self, req: Request<B>) -> Self::Future {
-        // TODO
-        // Log to file
-        // println!("Logger middleware triggered");
-        LoggingFuture {
+        AccessLog {
+            method: &req.method(),
+            uri: &req.uri(),
+            version: &req.version(),
+            headers: &req.headers(),
+        }
+        .write();
+
+        MiddlewareFuture {
             future: self.inner.call(req),
         }
     }
