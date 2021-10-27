0cc587131f77b7f1f12c9709f2ce41218f46281d
Onur Ozkan <onurozkan.dev@outlook.com>
2021-10-27 16:21:05 +0300

    create RouterService

--- network-utils/proxy/src/service.rs
+++ network-utils/proxy/src/service.rs
@@ -4,6 +4,8 @@ use hyper::{header::HeaderName, Body, Client, Request, Response, StatusCode};
 use std::{convert::Infallible, pin::Pin, task::Poll};
 use tower::Service;
 
+use crate::router::RouterService;
+
 #[derive(Clone)]
 /// Middleware service that can route and proxy between
 /// two connections.
@@ -26,64 +28,40 @@ impl Service<Request<Body>> for ProxyService {
     }
 
     fn call(&mut self, mut req: Request<Body>) -> Self::Future {
-        // Routing
-        let mut target;
-        let mut index = self.routes.iter().position(|r| {
-            r.inbound_route == req.uri().to_string()
-                || r.inbound_route.to_owned() + "/" == req.uri().to_string()
-        });
+        let route_dependencies = RouterService {}.get_dependencies(req.uri().to_string(), &self.routes);
 
-        if index.is_some() {
-            target = self.routes[index.unwrap()].dest_addr.to_string();
-        } else {
-            index = self.routes.iter().position(|r| r.inbound_route == "/");
+        // Return 404 if route doesn't exists
+        if route_dependencies.dest_addr.is_empty() {
+            return Box::pin(async {
+                let mut res = Response::new(Body::from(super::NOT_FOUND_BODY));
 
-            // Return 404 if requested route doesn't exists
-            if index.is_none() {
-                return Box::pin(async {
-                    let mut res = Response::new(Body::from(super::NOT_FOUND_BODY));
+                *res.status_mut() = StatusCode::NOT_FOUND;
+                res.headers_mut()
+                    .insert("content-type", "application/json".parse().unwrap());
 
-                    *res.status_mut() = StatusCode::NOT_FOUND;
-                    res.headers_mut()
-                        .insert("content-type", "application/json".parse().unwrap());
-
-                    return Ok(res);
-                });
-            }
-
-            target = self.routes[index.unwrap()].dest_addr.to_string();
-            target.push_str(&req.uri().to_string());
+                return Ok(res);
+            });
         }
-
-        if target.chars().last() != Some('/') {
-            target.push('/');
-        }
-        let index = index.unwrap();
 
         // Request header manipulation
-        self.routes[index]
-            .req_headers
-            .iter()
-            .flatten()
-            .for_each(|header| {
-                if header.value.is_some() {
-                    req.headers_mut().insert(
-                        HeaderName::from_bytes(header.key.as_bytes()).unwrap(),
-                        header.value.as_ref().unwrap().parse().unwrap(),
-                    );
-                } else {
-                    req.headers_mut().remove(&header.key);
-                }
-            });
+        route_dependencies.req_headers.iter().flatten().for_each(|header| {
+            if header.value.is_some() {
+                req.headers_mut().insert(
+                    HeaderName::from_bytes(header.key.as_bytes()).unwrap(),
+                    header.value.as_ref().unwrap().parse().unwrap(),
+                );
+            } else {
+                req.headers_mut().remove(&header.key);
+            }
+        });
 
-        let res_headers = self.routes[index].res_headers.clone();
         Box::pin(async move {
             let client = Client::new();
-            *req.uri_mut() = target.parse().unwrap();
+            *req.uri_mut() = route_dependencies.dest_addr.parse().unwrap();
             let mut res = client.request(req).await.unwrap();
 
             // Response header manipulation
-            res_headers.iter().flatten().for_each(|header| {
+            route_dependencies.res_headers.iter().flatten().for_each(|header| {
                 if header.value.is_some() {
                     res.headers_mut().insert(
                         HeaderName::from_bytes(header.key.as_bytes()).unwrap(),
